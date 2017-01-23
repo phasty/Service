6d73145b3ac5f6bc2af10956b0002a61f5342362
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2017-01-23 16:06:16 +1000

    Еще для тестов и совместимости с контроллером шины

--- src/Router.php
+++ src/Router.php
@@ -12,6 +12,18 @@ namespace Phasty\Service {
      */
     class Router {
 
+        protected static $routes = [];
+        protected static $format = "application/json";
+
+        /**
+         * Устанавливает конфигурацию роутинга
+         *
+         * @param  array  $routeMappings  Массив ключ(uri) => [class, method]
+         */
+        public static function init(array $routeMappings) {
+            static::$routes = $config;
+        }
+
         /**
          * Находит класс-обработчик и метод на основании запрошенного uri и конфига
          *
@@ -20,11 +32,11 @@ namespace Phasty\Service {
          *
          * @return array  Класс и метод для обработки запроса
          */
-        protected static function getClassAndMethod($requestedUri, array $routeMappings) {
-            if (empty($routeMappings[ $requestedUri ])) {
+        protected static function getClassAndMethod($requestedUri) {
+            if (empty(static::$routes[ $requestedUri ])) {
                 throw new Exception\ApiNotImplemented("Неизвестный ресурс '$requestedUri'.");
             }
-            return $routeMappings[ $requestedUri ];
+            return static::$routes[ $requestedUri ];
         }
 
         /**
@@ -34,28 +46,69 @@ namespace Phasty\Service {
          * Такая реализация нужна для более простого unit-тестирования.
          *
          * @param  string $requestedUri   Запрошенный uri
-         * @param  array  $input          Входяций набор данных
+         * @param  mixed  $input          Входяций набор данных
          * @param  array  $settings       Конфигурация для сервиса
          *
          * @return array  Результат обработки запроса
          */
-        protected static function getResult($requestedUri, array $input, array $settings) {
-            list($class, $method) = static::getClassAndMethod($requestedUri, $settings[ "routes" ]);
+        protected static function getResult($requestedUri, $input) {
+            list($class, $method) = static::getClassAndMethod($requestedUri);
             $instance = new $class;
             return $instance->$method($input);
         }
 
         /**
-         * Обрабатывает запрос и возвращает http-ответ в виде json-данных
+         * Метод ожидает json-объект на входном потоке данных (в теле http-запроса)
+         * и пытается рапарсить его
          *
-         * @param  array  $settings       Конфигурация для сервиса
+         * @return array  Ассоциативный массив данных из входящего запроса
          */
-        public static function route(array $settings) {
-            $requestedUri = $_SERVER[ "PHP_SELF" ];
+        public static function getData() {
+            static $result = null;
+            if (!isset($result)) {
+                $result = file_get_contents("php://input");
+                if (static::isJson()) {
+                    $result = ($result) ? json_decode($result, true) : [];
+                }
+            }
+            return $result;
+        }
+
+        /**
+         * Устанавливает формат ответа.
+         * По умолчанию формат ответа совпадает с форматом запроса.
+         *
+         * @param string $contentType Формат в виде MIME-type
+         */
+        public static function setFormat($contentType) {
+            static::$format = empty($contentType) ? "application/json" : $contentType;
+        }
+
+        /**
+         * Проверяет что текущий формат - json
+         *
+         * @return boolean
+         */
+        protected static function isJson() {
+            return ("application/json" == static::$format) ? true : false;
+        }
+
+        /**
+         * Обрабатывает запрос и возвращает http-ответ в виде:
+         *  json-данных - когда $_SERVER["CONTENT_TYPE"] == "application/json"
+         *  сырой результат обработки, если входящий запрос не "application/json"
+         *
+         * @param string $requestedUri  Входящий uri - эквивалентен $_SERVER[ "PHP_SELF" ]
+         */
+        public static function route($requestedUri) {
             try {
                 // Копим весь прямой вывод (ошибки, случайное echo от разработчика и т.д.)
                 ob_start();
-                $result = getResult($requestedUri, (new Input)->getData(), array $settings);
+                static::setFormat($_SERVER["CONTENT_TYPE"]);
+                $result = getResult($requestedUri, static::getData());
+                if (!is_array($result)) {
+                    throw new Exception\InternalError("Результат обработки '$requestedUri' не array.");
+                }
                 // Чистим весь левый вывод. Мы должны отдать только результат!
                 ob_end_clean();
             } catch (\Exception $e) {
@@ -66,9 +119,9 @@ namespace Phasty\Service {
                 $result = ["code" => $e->getCode(), "message" => $e->getMessage()];
             }
 
-            header("Content-Type: application/json");
+            header("Content-Type: " . static::$format);
             header("Content-Length: " . strlen($result));
-            echo json_encode($result);
+            echo static::isJson() ? json_encode($result) : $result;
         }
 
     }
